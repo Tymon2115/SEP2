b8e89b7abaed6dc4def6417ade373d20e426fac6
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-05-13 19:10:51 +0200

    added sql code

---
+++ databasecode.sql
@@ -0,0 +1,68 @@
+CREATE SCHEMA SEP;
+
+SET SCHEMA 'sep';
+
+--entities
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY ,
+    role_name VARCHAR (50)
+);
+
+CREATE TABLE branch (
+    id INTEGER PRIMARY KEY ,
+    name VARCHAR (500) NOT NULL,
+    location VARCHAR (100)
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(800) NOT NULL,
+    surname VARCHAR (40) NOT NULL,
+    role_id INTEGER,
+    branch_id INTEGER,
+    username VARCHAR (30) NOT NULL,
+    password VARCHAR NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (branch_id) REFERENCES branch (id)
+
+);
+
+
+
+
+CREATE TABLE car (
+    id INTEGER PRIMARY KEY ,
+    make VARCHAR (30) NOT NULL ,
+    model VARCHAR (50) NOT NULL ,
+    color VARCHAR (30) NOT NULL ,
+    number_plates VARCHAR (10) NOT NULL ,
+    fuel_type VARCHAR (15) NOT NULL ,
+    fuel_consumption VARCHAR (15) NOT NULL ,
+    seats INTEGER NOT NULL ,
+    engine VARCHAR (30) NOT NULL ,
+    transmission VARCHAR (50) NOT NULL ,
+    equipment VARCHAR,
+    description VARCHAR,
+    branch_id INTEGER,
+    FOREIGN KEY (branch_id) REFERENCES branch (id)
+);
+
+CREATE TABLE reservation (
+    id INTEGER PRIMARY KEY ,
+    name VARCHAR (800) NOT NULL ,
+    surname VARCHAR (40) NOT NULL ,
+    drivers_licence VARCHAR (50) NOT NULL ,
+    address_street VARCHAR (100) NOT NULL ,
+    address_city VARCHAR (100) NOT NULL ,
+    address_zip VARCHAR (12) NOT NULL ,
+    address_country VARCHAR (100) NOT NULL ,
+    car_id INTEGER NOT NULL ,
+    start_branch_id INTEGER NOT NULL ,
+    end_branch_id INTEGER NOT NULL ,
+    start_date DATE NOT NULL ,
+    end_date DATE NOT NULL ,
+    FOREIGN KEY (car_id) references car (id),
+    FOREIGN KEY (start_branch_id) references branch (id),
+    FOREIGN KEY (end_branch_id) references  branch (id)
+);
