e44cdce9bc3abd9ff1db0beeb5cbd0c17189fd8c
sperlik-oliver <75211235+sperlik-oliver@users.noreply.github.com>
2021-05-18 15:20:56 +0200

    more database handlers

--- code/src/server/database/BranchHandler.java
+++ code/src/server/database/BranchHandler.java
@@ -1,35 +1,83 @@
 package server.database;
 
+import shared.Branches.Branch;
+import shared.Reservation.Cars;
+import shared.Reservation.Reservations;
+import shared.personel.Employees;
+import shared.personel.Manager;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BranchHandler {
 
-    private Connection databaseConnection = DatabaseConnection.getInstance().getConnection();
-    private Statement statement;
+    private final Connection connection = DatabaseConnection.getInstance().getConnection();
 
-    public Branch createBranch () {
+
+    public void createBranch (String name, String location) {
         try {
-            Statement statement = databaseConnection.createStatement();
-            ResultSet result = statement.executeQuery("");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO 'branch' (name, location) VALUES ("+ name + "," + location + ");" );
+            statement.close();
         } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
 
+            throwables.printStackTrace();
+
+        }
     }
 
-    public Branch getBranch(int id){
+    public Branch getBranch(int searchId){
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM 'branch' WHERE id = " + searchId + " ); ");
+            int id;
+            String name;
+            String location;
+            while (result.next()) {
+                id = result.getInt("id");
+                name = result.getString("name");
+                location = result.getString("location");
+            }
+            statement.close();
+            result.close();
+            Branch branch = new Branch(id, name, location);
+            return branch;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
 
+        }
+        return null;
     }
 
     public ArrayList<Branch> getBranch(){
+        try {
+            Statement statement = databaseConnection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM 'branch'");
+            Arraylist<Branch> branches = new ArrayList<>();
+            while (result.next()) {
 
+            }
+       return null;
+        }
+        catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
     }
 
-    public Branch editBranch(int id) {
+    public void editBranch(int id, String name, String location, Employees employees, Reservations reservations, Cars cars, Manager manager) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("");
+            statement.close();
+        } catch (SQLException throwables) {
 
+            throwables.printStackTrace();
+
+        }
     }
 
 }
