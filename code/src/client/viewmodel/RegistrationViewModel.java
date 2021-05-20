9938abba2017c44a7fd76585f27060cc3f6cfddb
Boierul <pintea.dan988@gmail.com>
2021-05-20 12:43:52 +0200

    Today

---
+++ code/src/client/viewmodel/RegistrationViewModel.java
@@ -0,0 +1,96 @@
+package client.viewmodel;
+
+import client.model.DataModel;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import shared.personel.Employee;
+
+import java.beans.PropertyChangeSupport;
+
+public class RegistrationViewModel {
+
+    private DataModel model;
+    private StringProperty firstname, lastname, username, password, confirmpassword, registrationMessageLabel;
+    private PropertyChangeSupport support;
+
+    public RegistrationViewModel(DataModel model) {
+        this.model = model;
+        support = new PropertyChangeSupport(this);
+
+        firstname = new SimpleStringProperty();
+        lastname = new SimpleStringProperty();
+        username = new SimpleStringProperty();
+        password = new SimpleStringProperty();
+        confirmpassword = new SimpleStringProperty();
+        registrationMessageLabel = new SimpleStringProperty();
+
+        // Branch and Role
+
+    }
+
+    public void register() {
+        if (firstname.get() == null || "".equals(firstname.get())) {
+            registrationMessageLabel.setValue("Please input your first name");
+        } else if (lastname.get() == null || "".equals(lastname.get())) {
+            registrationMessageLabel.setValue("Please input your last name");
+        } else if (username.get() == null || "".equals(username.get())) {
+            registrationMessageLabel.setValue("Please input your username");
+        } else if (password.get() == null || "".equals(password.get())) {
+            registrationMessageLabel.setValue("Please input your password");
+        } else if (password.get().length() < 3 || password.get().length() > 15) {
+            registrationMessageLabel.setValue("Password needs to be between 3 and 15 characters");
+        } else if (confirmpassword.get() == null || "".equals(confirmpassword.get())) {
+            registrationMessageLabel.setValue("Please input your password confirmation");
+        } else {
+            if (confirmpassword.get() != null) {
+                if (!confirmpassword.get().equals(password.get())) {
+                    registrationMessageLabel.setValue("The password don't match");
+                } else {
+                    registerEmployeeAccount();
+
+                }
+            }
+        }
+    }
+
+    public void registerEmployeeAccount() {
+        model.register(new Employee(firstname.get(), lastname.get(), username.get(), password.get()));
+        defaultFields();
+    }
+
+    public void defaultFields() {
+        firstname.setValue("");
+        lastname.setValue("");
+        username.setValue("");
+        password.setValue("");
+        confirmpassword.setValue("");
+        registrationMessageLabel.setValue("");
+    }
+
+    public StringProperty firstnameProperty() {
+        return firstname;
+    }
+
+    public StringProperty lastnameProperty() {
+        return lastname;
+    }
+
+    public StringProperty usernameProperty() {
+        return username;
+    }
+
+
+    public StringProperty passwordProperty() {
+        return password;
+    }
+
+
+    public StringProperty confirmpasswordProperty() {
+        return confirmpassword;
+    }
+
+
+    public StringProperty registrationMessageLabelProperty() {
+        return registrationMessageLabel;
+    }
+}
