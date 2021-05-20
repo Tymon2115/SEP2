ebaa3b1b78dddd36b2ed3068d98f115356792a1e
Tymon2115 <tymon.podlowski@gmail.com>
2021-05-20 13:07:17 +0200

    yikes

--- code/src/client/viewmodel/CarViewModel.java
+++ code/src/client/viewmodel/CarViewModel.java
@@ -1,33 +1,33 @@
-package client.viewmodel;
-
-import client.model.Model;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import shared.Reservation.Car;
-
-public class CarViewModel {
-
-    public StringProperty usernameProperty;
-    private Model model;
-
-    public CarViewModel(Model model) {
-        this.model = model;
-        usernameProperty = new SimpleStringProperty();
-    }
-
-    public StringProperty usernamePropertyProperty() {
-        return usernameProperty;
-    }
-
+//package client.viewmodel;
+//
+//import client.model.Model;
+//import javafx.beans.property.SimpleStringProperty;
+//import javafx.beans.property.StringProperty;
+//import shared.Reservation.Car;
+//
+//public class CarViewModel {
+//
+//    public StringProperty usernameProperty;
+//    private Model model;
+//
+//    public CarViewModel(Model model) {
+//        this.model = model;
+//        usernameProperty = new SimpleStringProperty();
+//    }
+//
+//    public StringProperty usernamePropertyProperty() {
+//        return usernameProperty;
+//    }
+//
 //    public void addCarView(Car car) {
 //        model.createCar(car);
 //    }
-
-    public void deleteCarView(Car car) {
-        model.deleteCar(car);
-    }
-
+//
+//    public void deleteCarView(Car car) {
+//        model.deleteCar(car);
+//    }
+//
 //    public void openEditCarView(Car car) {
 //        model.editCar(car);
 //    }
-}
+//}
